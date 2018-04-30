40f821516f0c5d77516f554ee35ee3e7c9bae89c
diemanmanly <dieman.manly@gmail.com>
2018-04-30 16:32:24 +0700

    menu

--- App.js
+++ App.js
@@ -1,33 +1,61 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, Image} from 'react-native';
 import Login from './app/components/login';
-import { Router, Scene } from 'react-native-router-flux';
+import HomeScreen from './app/screens/home';
+import SettingsScreen from './app/screens/settings';
+// import {Router, Scene} from 'react-native-router-flux';
+import {DrawerNavigator, DrawerItems} from 'react-navigation';
+import {Icon, Button, Container, Header, Content, Left, Body} from 'native-base';
 
 export default class App extends React.Component {
-  render() {
-    return (
-        <Router>
-            <Scene
-                key="login"
-                component={Login}
-                title="Login"
-                initial
-            >
+    render() {
+        return (
+            <MyApp/>
 
-            </Scene>
-        </Router>
-      //   <View style={styles.container}>
-      //     <Login />
-      // </View>
-    );
-  }
+        );
+    }
 }
 
+const customDrawerContentComponent = (props) => (
+    <Container>
+        <Header style={{height: 200, backgroundColor: 'white'}}>
+            <Body>
+            <Image
+                style={styles.drawerImage}
+                source={require('./app/assets/images/unnamed.jpg')}/>
+            </Body>
+        </Header>
+        <Content>
+            <DrawerItems {...props} />
+        </Content>
+    </Container>
+)
+
+const MyApp = DrawerNavigator({
+    Home: {
+        screen: HomeScreen
+    },
+    Setting: {
+        screen: SettingsScreen
+    }
+}, {
+    initialRouteName: 'Home',
+    drawerPosition: "Left",
+    contentComponent: customDrawerContentComponent,
+    drawerOpenRoute: 'DrawerOpen',
+    drawerCloseRoute: 'DrawerClose',
+    drawerToggleRoute: 'DrawerToggle'
+});
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    drawerImage: {
+        height: 150,
+        width: 150,
+        borderRadius: 75
+    }
 });
